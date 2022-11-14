bd0f39f0bf1664278e46ca7f1798ee4f8c26a25a
Umar <umarg1997@gmail.com>
2022-11-14 17:44:08 +0000

    update

--- primitives/primitives.rs
+++ primitives/primitives.rs
@@ -13,4 +13,14 @@ fn main() {
   // mut keyword is used to make a variable mutable
   let mut inferred_type = 12;
   inferred_type = 4294967296i64;
+
+  // A mutable variable's value can be changed
+  let mut mutable = 12;
+  mutable = 21;
+
+  // Error! The type of a variable can't be changed
+  mutable = true;
+
+  // Variables can be overwritten with shadowing
+  let mutable = true;
 }
